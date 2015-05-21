54e5b0833ba3f6f11e539eb41419e34d09464d06
Henry Ramirez <h.ramirez0427@gmail.com>
2015-05-21 09:15:04 -0700

    commit 3 adding in some php

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
-<!DOCTYPE html>
-<html>
-<head>
-	 <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
-   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-   <link rel="stylesheet" type="text/css" href="css/main.css">
-   <meta name="viewport" content="width=device-width">
-   <meta name="viewport" content='width=320'>
-	<title>Henry's Landing page</title>
-</head>
+<?php 
+require_once(__DIR__ . "view/header.php");
+?>
 <body>
+<div class="container">
+	<h1>Landing Page</h1>
+		<div class="row">
+			<div class="col-xs-10">
+			</div>
+			<div class="col-xs-2">
+				   <a href="#" class="listed-group">
+       <a href="" class="list-group-item"></a>
+       <a href=""class="list-group-item"></a>
+       <a href="#" class="list-group-item"></a>
+       <a href="#" class="list-group-item"></a>
+      </a>
+			</div>
+		</div>
+</div>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
   <script type="text/javascript" src="js/bootstrap.min.js"> </script>
-</body>
-</html>
+  <script type="text/javascript" src="js/main.js"></script>
+<?php 
+require_once(__DIR__ . "view/footer.php");
+?>
---
+++ view/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	 <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+   <link rel="stylesheet" type="text/css" href="css/main.css">
+   <meta name="viewport" content="width=device-width">
+   <meta name="viewport" content='width=320'>
+	<title>Henry's Landing page</title>
+</head>
+<body>
