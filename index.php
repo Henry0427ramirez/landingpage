e687e2326f77b3a3b820cf179b9f1fded1fd1fce
Henry Ramirez <h.ramirez0427@gmail.com>
2015-05-21 08:53:37 -0700

    commit 2 done with addding bootstrap

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	 <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+   <link rel="stylesheet" type="text/css" href="css/main.css">
+   <meta name="viewport" content="width=device-width">
+   <meta name="viewport" content='width=320'>
+	<title>Henry's Landing page</title>
+</head>
+<body>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"> </script>
+</body>
+</html>
