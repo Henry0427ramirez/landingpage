efc4fa880ccdb65700a6f5385cb969e4365694ca
Henry Ramirez <h.ramirez0427@gmail.com>
2015-05-27 09:03:05 -0700

    commit 5

--- view/header.php
+++ view/header.php
@@ -7,5 +7,45 @@
    <meta name="viewport" content="width=device-width">
    <meta name="viewport" content='width=320'>
 	<title>Henry's Landing page</title>
-</head>
+      <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#"></a>
+    </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li class="divider"></li>
+            <li><a href="#"></a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<header>
+	<audio controls>
+  <source src="music/$aturn-Clift-Hanger.mp3" type="audio/mpeg">
+  <source src="music/AWE-Crystals.mp3" type="audio/mpeg">
+</audio>
+</header>
 <body>
