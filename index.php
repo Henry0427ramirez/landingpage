b7a5be6d0662b788cdb0dddab77db0194c7f8a11
Henry Ramirez <h.ramirez0427@gmail.com>
2015-05-27 09:02:36 -0700

    commit 4

--- index.php
+++ index.php
@@ -1,25 +1,22 @@
 <?php 
-require_once(__DIR__ . "view/header.php");
+require_once(__DIR__ . "/view/header.php");
 ?>
 <body>
 <div class="container">
-	<h1>Landing Page</h1>
-		<div class="row">
+	<h1></h1>
+	<!--	<div class="row">
 			<div class="col-xs-10">
 			</div>
 			<div class="col-xs-2">
 				   <a href="#" class="listed-group">
        <a href="" class="list-group-item"></a>
        <a href=""class="list-group-item"></a>
-       <a href="#" class="list-group-item"></a>
-       <a href="#" class="list-group-item"></a>
+       <a href="" class="list-group-item"></a>
+       <a href="" class="list-group-item"></a>
       </a>
 			</div>
-		</div>
+		</div> -->
 </div>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-  <script type="text/javascript" src="js/bootstrap.min.js"> </script>
-  <script type="text/javascript" src="js/main.js"></script>
 <?php 
-require_once(__DIR__ . "view/footer.php");
+require_once(__DIR__ . "/view/footer.php");
 ?>
